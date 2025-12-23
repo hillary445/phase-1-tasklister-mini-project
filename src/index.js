120caa40603a4c08c2eebc8b2ed6954b8252a4ac
hillary445 <hilaryprit02@gmail.com>
2025-12-24 01:48:16 +0300

    A complete tasklister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+const taskForm = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+taskForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const taskInput = document.querySelector('#new-task-description');
+  const taskDescription = taskInput.value;
+
+  buildToDo(taskDescription);
+
+  taskInput.value = '';
+});
+
+function buildToDo(task) {
+  const li = document.createElement('li');
+
+  li.textContent = task;
+
+  taskList.appendChild(li);
+}
 });
